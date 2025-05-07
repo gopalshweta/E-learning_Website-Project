61c9ab2b30815e28df70aef242855f288cb7216e
Shweta Gopal <116021306+gopalshweta@users.noreply.github.com>
2025-05-07 21:45:50 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,25 @@
+ <!--Start Contact Us-->
+ <div class="container mt-4" id="Contact"> 
+    <h2 class="text-center mb-4">Contact US</h2> 
+    <div class="row">  
+      <div class="col-md-8"> 
+        <form action="" method="post">
+          <input type="text" class="form-control" name="name" placeholder="Name"><br>
+          <input type="text" class="form-control" name="subject" placeholder="Subject"><br>
+          <input type="email" class="form-control" name="email" placeholder="E-mail"><br>
+          <textarea class="form-control" name="message" placeholder="How can we help you?" style="height:150px;"></textarea><br>
+          <input class="btn btn-primary" type="submit" value="Send" name="submit"><br><br>
+        </form>
+      </div> 
+
+      <div class="col-md-4 stripe text-white text-center">  
+        <h4>iSchool</h4>
+        <p>iSchool, 
+        Near Police Camp II, Borivali West, 
+        Mumbai - 400091 <br /> 
+        Phone: +00000000 <br />
+        www.ischool.com </p>
+      </div> 
+    </div> 
+  </div> 
+  <!-- End Contact Us -->
